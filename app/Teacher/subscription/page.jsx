b31afc440a3e9c8438597c3b591e43a518590b20
filopa <85035598+filopa@users.noreply.github.com>
2026-02-25 14:21:59 +0200

    Fix merge conflict in Subscriptions component

--- app/Teacher/subscription/page.jsx
+++ app/Teacher/subscription/page.jsx
@@ -3,10 +3,7 @@ import React from 'react'
 function Subscriptions() {
     return (
         <>
-<<<<<<< HEAD
-=======
           
->>>>>>> b2e5d71450436003e1b10a8d7ad144413c22186f
             <div>Subscriptions</div>
         </>
     )
